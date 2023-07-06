565161b5ca3d40f5d1caa1614cf3a22bd11e90cb
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-07-06 16:51:06 +0700

    update 5

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -3,13 +3,17 @@ package com.example.myapplication
 import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.dinuscxj.progressbar.CircleProgressBar
+import java.text.DecimalFormat
 
 class MainActivity3 : AppCompatActivity() {
     private lateinit var mCircleProgressBar: CircleProgressBar
     private lateinit var imageViewNeedle: ImageView
+    private lateinit var tvValueSpeed: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +21,11 @@ class MainActivity3 : AppCompatActivity() {
         setContentView(R.layout.activity_main3)
         mCircleProgressBar = findViewById(R.id.my_cpb)
         imageViewNeedle = findViewById(R.id.ivThumb)
+        tvValueSpeed = findViewById(R.id.tvValueSpeed)
 
 
 
-        mCircleProgressBar.max = 123
+        mCircleProgressBar.max = 148
 
         mCircleProgressBar.isDrawBackgroundOutsideProgress = true
         mCircleProgressBar.setProgressBackgroundColor(
@@ -36,11 +41,14 @@ class MainActivity3 : AppCompatActivity() {
 
 
 
-        val animatorValueOnGauge = ValueAnimator.ofInt(0, 100)
+        val animatorValueOnGauge = ValueAnimator.ofFloat(0f, 120f)
         animatorValueOnGauge.addUpdateListener { animation ->
-            val progress = animation.animatedValue as Int
+            val progress = (animation.animatedValue as Float).toInt()
             mCircleProgressBar.progress = progress
-            imageViewNeedle.rotation = (2.9 * progress).toFloat()
+            imageViewNeedle.rotation = (2.43 * progress).toFloat()
+            val decimalFormat = DecimalFormat("0.00")
+            val formattedNumber = decimalFormat.format(animation.animatedValue)
+            tvValueSpeed.text = formattedNumber
         }
         animatorValueOnGauge.repeatCount = ValueAnimator.INFINITE
         animatorValueOnGauge.duration = 20000
